f3eddd98002f759683e82ba22794b2160cb376a2
John Atkins <johnatkins1999@yahoo.com>
2017-07-24 16:46:53 -1000

    2nd

--- joins.sql
+++ joins.sql
@@ -10,8 +10,21 @@
 -- LEFT JOIN users u ON p.users_id = u.id
 -- WHERE p.users_id = 200;
 
-SELECT u.username, p.title, p.url, p.content, p.created_at, p.updated_at
-FROM posts p
-LEFT JOIN users u ON p.users_id = u.id
-WHERE u.first_name = 'Norene' AND u.last_name = 'Schmitt';
+-- SELECT u.username, p.title, p.url, p.content, p.created_at, p.updated_at
+-- FROM posts p
+-- LEFT JOIN users u ON p.users_id = u.id
+-- WHERE u.first_name = 'Norene' AND u.last_name = 'Schmitt';
 
+-- SELECT u.username, p.created_at
+-- FROM users u
+-- LEFT JOIN posts p ON p.users_id = u.id
+-- WHERE p.created_at > '20150101';
+
+-- SELECT p.title, p.content, u.username
+-- FROM users u
+-- LEFT JOIN posts p ON p.users_id = u.id
+-- WHERE u.created_at < '20150101';
+
+SELECT p.title as "POST TITLE", c.body, c.created_at, c.updated_at
+FROM comments c
+LEFT JOIN posts p ON c.posts_id = p.id;
